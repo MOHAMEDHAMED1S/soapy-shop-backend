cd3bc3ca33b3cbc2b101dac9521d6b980592f071
Mohamed Hamed <163942536+MOHAMEDHAMED1S@users.noreply.github.com>
2025-10-12 01:12:50 +0300

    .

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'frontend_url' => env('FRONTEND_URL', 'http://localhost:8080'),
+    'frontend_url' => env('FRONTEND_URL', 'https://soapy-bubbles.com'),
 
     /*
     |--------------------------------------------------------------------------
